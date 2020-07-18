f9dbc34775f35ef55553826297b1e2b05e42ff45
brmagrini@gmail.com <brmagrini@gmail.com>
2020-07-18 18:12:51 -0300

    adiciona tabelas

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Fundo(models.Model):
+    cnpj = models.CharField(max_length=200)
+    resgate = models.IntegerField()
+
+class Arquivos(models.Model):
+    nome = models.CharField(max_length=200)
+    tamanho = models.IntegerField()
