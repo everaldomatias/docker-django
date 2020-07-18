7dc6a035219e8ee4801bfb17ebace7d6f0fbfba6
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2020-07-18 16:05:30 -0300

    arquivos urls e views da pasta core atualizados e arquivo contato html criado

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-# def contato(request):
-#    return render(request, 'contato.html')
+def contato(request):
+   return render(request, 'contato.html')
 
 
