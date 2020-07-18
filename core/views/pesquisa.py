e88ebdc95d26d4e9ca0fca5c61ec0156d0a8110d
brmagrini@gmail.com <brmagrini@gmail.com>
2020-07-18 20:27:37 -0300

    munda nome do campo no form

--- core/views/pesquisa.py
+++ core/views/pesquisa.py
@@ -3,14 +3,22 @@ from ..helpers import cnpj as cn
 from core.models import Fundo
 
 def pesquisa(request):
-    cnpj = request.POST.get('current_string') if request.POST.get('current_string') else ''
-    cnpj_format = cn.Cnpj().format(cnpj.replace('/','').replace('-','').replace('.',''))
-    print(cnpj_format)
+    cnpj = request.POST.get('cnpj') if request.POST.get('cnpj') else ''
+    cnpj_format = ''
+    if (cnpj.replace('/','').replace('-','').replace('.','') != ''):
+        cnpj_format = cn.Cnpj().format(cnpj.replace('/','').replace('-','').replace('.',''))
     fundo = Fundo.objects.filter(cnpj=cnpj_format)
-    resgate = fundo[0].resgate if hasattr(fundo[0],'resgate') else ''
-    cnpj = fundo[0].cnpj if hasattr(fundo[0],'cnpj') else ''
+    if (fundo.count() > 0):
+        resgate = fundo[0].resgate if hasattr(fundo[0],'resgate') else ''
+        cnpj = fundo[0].cnpj if hasattr(fundo[0],'cnpj') else ''
+        context = {
+            'resgate': resgate,
+            'cnpj': cnpj
+        }
+        return render(request, 'pesquisa.html', context)
+    resgate = 'não existe no banco' if cnpj_format != '' else ''
     context = {
-        'resgate': resgate,
-        'cnpj': cnpj
+        'cnpj': cnpj_format,
+        'resgate': resgate
     }
     return render(request, 'pesquisa.html', context)
