f7182c7c1e98e3fdf66d506d16527407efab1e6c
everaldomatias <matiaseveraldo@gmail.com>
2020-07-18 17:39:51 -0300

    Add new version from views, based on folder and one example from form search.

--- core/views/index.py
+++ core/views/index.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
     context = {
         'descricao': 'Sistema de consulta de dados',
         'empresa': 'Pele Vermeleha',
     }
     return render(request, 'index.html', context)
-
-def contato(request):
-   return render(request, 'contato.html')
-
-
