ce5812bd7692c9393956875c95fa6115d12ede8f
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2020-08-01 15:56:45 -0300

    atulaização do pesquisa.py e pesquisa.html integração com o banco

--- core/views/pesquisa.py
+++ core/views/pesquisa.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render
 from ..helpers import cnpj as cn
-from core.models import Fundo
+from core.models import Dados
+
 
 def pesquisa(request):
     cnpj = request.POST.get('cnpj') if request.POST.get('cnpj') else ''
-    cnpj_format = ''
-    if (cnpj.replace('/','').replace('-','').replace('.','') != ''):
-        cnpj_format = cn.Cnpj().format(cnpj.replace('/','').replace('-','').replace('.',''))
-    fundo = Fundo.objects.filter(cnpj=cnpj_format)
-    if (fundo.count() > 0):
-        resgate = fundo[0].resgate if hasattr(fundo[0],'resgate') else ''
-        cnpj = fundo[0].cnpj if hasattr(fundo[0],'cnpj') else ''
-        context = {
-            'resgate': resgate,
-            'cnpj': cnpj
-        }
-        return render(request, 'pesquisa.html', context)
-    resgate = 'não existe no banco' if cnpj_format != '' else ''
     context = {
-        'cnpj': cnpj_format,
-        'resgate': resgate
+        'itens': Dados.objects.filter(cnpj=cnpj),
+        'cnpj': cnpj
     }
+
     return render(request, 'pesquisa.html', context)
+
+
+
